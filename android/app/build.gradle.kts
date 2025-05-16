a4104a25d6a95763bdf70fd8288f11c992535ae2
ProZobaer <zobaer.contact@gmail.com>
2025-05-16 15:13:15 +0600

    ok 2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("com.google.gms.google-services")  // Add this plugin here
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -20,25 +20,15 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.spms_v1"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
-    }
-
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
+        versionCode = 1
+        versionName = "1.0"
     }
 }
 
-flutter {
-    source = "../.."
+dependencies {
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
+    // You can add other Firebase dependencies here if needed
 }
